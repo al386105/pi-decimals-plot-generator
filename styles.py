598ed0af972e6c649f7812b84932b701362a2859
mpardo <al386105@uji.es>
2022-12-11 23:12:30 +0100

    added runner

--- styles.py
+++ styles.py
@@ -1,4 +1,4 @@
-show_plots_title = True
+show_plots_title = False
 color_lines = ['#5383EC', '#D85040', '#F2BF41', '#34A853', '#FF6D01', '#46BDC6', '#9900FF', '#804000', '#FF0080', '#000000']
 marker_styles = ['o', '^', 's', 'd', 'p', 'h', '*', 'x', '+', '1']
 font_title = {'family': 'serif', 'color': 'black', 'weight': 'bold', 'size': 13}
