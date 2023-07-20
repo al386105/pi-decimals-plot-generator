a912b651a31932e1b928bc7268e749f1b0b4cc89
mipardo <mipardo@uji.es>
2023-07-20 12:27:05 +0200

    refactor: added color and style to mpi comparison table

--- latex_table_generator_mpi_comparison.py
+++ latex_table_generator_mpi_comparison.py
@@ -8,11 +8,11 @@ def get_execution_times_latex_table(results):
     for algorithm_key in results.keys():
         if algorithm_key not in mpi_algorithms_excluded:
             times = list(results[algorithm_key][default_comparison_precision].values())
-            row = "\t" + algorithm_key
+            row = "\t \\textbf{" + algorithm_key + "}"
             row += (24 - len(row)) * " "
             for i, time in enumerate(times):
                 if i * 10 in procs_to_show:
-                    string_time = f"& {round(time, 2)}"
+                    string_time = " & \multicolumn{1}{c|}{" + str(round(time, 2)) + "}"
                     string_time = string_time.replace('.', ',')
                     string_time += (12 - len(string_time)) * " "
                     row += string_time
@@ -26,10 +26,11 @@ def get_execution_times_latex_table(results):
                   + "{:,}".format(default_comparison_precision).replace(",", ".") + \
                   " decimales de precisión, utilizando el paradigma de programación con MPI}\n" \
                   "\\label{table:ex-mpi-comparison}\n" \
-                  "\\begin{tabular}{| c | c | c | c | c | c | c | c |}\n" \
+                  "\\begin{tabular}{|c|ccccccc|}\n" \
                   "\\hline\n" \
-                  "\t\\multirow{2}{*}{\\textbf{Algoritmo}} & \multicolumn{7}{ | c | }{\\textbf{Número de procesos}} \\\\\n" \
-                  "\t\\cline{2-8} & \\textbf{1} & \\textbf{10} & \\textbf{40} & \\textbf{80} & \\textbf{120} & \\textbf{160} & \\textbf{200} \\\\\n" \
+                  "\t\\cellcolor{Grey} & \\multicolumn{7}{c|}{\\cellcolor{Grey}\\textbf{Número de procesos}} \\\\\n" \
+                  "\t\\cline{2-8} \n" \
+                  "\t\\multirow{-2}{*}{\\cellcolor{Grey}\\textbf{Algoritmo}} & \\multicolumn{1}{c|}{\\textbf{1}} & \\multicolumn{1}{c|}{\\textbf{10}} & \\multicolumn{1}{c|}{\\textbf{40}} & \\multicolumn{1}{c|}{\\textbf{80}} & \\multicolumn{1}{c|}{\\textbf{120}} & \\multicolumn{1}{c|}{\\textbf{160}} & \\textbf{200} \\\\\n " \
                   "\t\\hline\n" \
                   + data_rows + \
                   "\\end{tabular}\n" \
@@ -44,11 +45,11 @@ def get_speed_ups_latex_table(results):
         if algorithm_key not in mpi_algorithms_excluded:
             times = list(results[algorithm_key][default_comparison_precision].values())
             speed_ups = [times[0] / times[i] for i in range(len(times))]
-            row = "\t" + algorithm_key
+            row = "\t \\textbf{" + algorithm_key + "}"
             row += (24 - len(row)) * " "
             for i, speed_up in enumerate(speed_ups):
                 if i * 10 in procs_to_show:
-                    string_su = f"& {round(speed_up, 2)}"
+                    string_su = " & \multicolumn{1}{c|}{" + str(round(speed_up, 2)) + "}"
                     string_su = string_su.replace('.', ',')
                     string_su += (12 - len(string_su)) * " "
                     row += string_su
@@ -62,10 +63,11 @@ def get_speed_ups_latex_table(results):
                   + str(default_comparison_precision) + \
                   " decimales de precisión, utilizando el paradigma de programación con MPI}\n" \
                   "\\label{table:su-mpi-comparison}\n" \
-                  "\\begin{tabular}{| c | c | c | c | c | c | c | c |}\n" \
+                  "\\begin{tabular}{|c|ccccccc|}\n" \
                   "\\hline\n" \
-                  "\t\\multirow{2}{*}{\\textbf{Algoritmo}} & \multicolumn{7}{ | c | }{\\textbf{Número de procesos}} \\\\\n" \
-                  "\t\\cline{2-8} & \\textbf{1} & \\textbf{10} & \\textbf{40} & \\textbf{80} & \\textbf{120} & \\textbf{160} & \\textbf{200} \\\\\n" \
+                  "\t\\cellcolor{Grey} & \\multicolumn{7}{c|}{\\cellcolor{Grey}\\textbf{Número de procesos}} \\\\\n" \
+                  "\t\\cline{2-8} \n" \
+                  "\t\\multirow{-2}{*}{\\cellcolor{Grey}\\textbf{Algoritmo}} & \\multicolumn{1}{c|}{\\textbf{1}} & \\multicolumn{1}{c|}{\\textbf{10}} & \\multicolumn{1}{c|}{\\textbf{40}} & \\multicolumn{1}{c|}{\\textbf{80}} & \\multicolumn{1}{c|}{\\textbf{120}} & \\multicolumn{1}{c|}{\\textbf{160}} & \\textbf{200} \\\\\n " \
                   "\t\\hline\n" \
                   + data_rows + \
                   "\\end{tabular}\n" \
